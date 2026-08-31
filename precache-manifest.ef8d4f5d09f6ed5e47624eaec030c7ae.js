905e75be679402b861686910f72b7c5399c7cb31
Engel Yu <engelcyy@gmail.com>
2026-09-01 02:55:42 +0800

    Deploy: seeded example plan, examples page removed

---
+++ precache-manifest.ef8d4f5d09f6ed5e47624eaec030c7ae.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6071db5b28a27cafe134fcdab1a82c78",
+    "url": "/nctucourse/index.html"
+  },
+  {
+    "revision": "de47cd7de4deac81cd1f",
+    "url": "/nctucourse/static/css/main.2d25cf44.chunk.css"
+  },
+  {
+    "revision": "d00b0f0e50a6d387dcae",
+    "url": "/nctucourse/static/js/2.d68cfd3d.chunk.js"
+  },
+  {
+    "revision": "7641b3e81503c5861af61807440883f2",
+    "url": "/nctucourse/static/js/2.d68cfd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de47cd7de4deac81cd1f",
+    "url": "/nctucourse/static/js/main.8ee4288a.chunk.js"
+  },
+  {
+    "revision": "977770627ee4db4586d3",
+    "url": "/nctucourse/static/js/runtime-main.3da64a01.js"
+  },
+  {
+    "revision": "5bddb30c681625b4002423078465f179",
+    "url": "/nctucourse/static/media/gpa_teach1.5bddb30c.png"
+  },
+  {
+    "revision": "c9a6945ea029483e9da50018b7397f9f",
+    "url": "/nctucourse/static/media/gpa_teach2.c9a6945e.png"
+  },
+  {
+    "revision": "33724e07b22343a03f27ed07cd53c2bd",
+    "url": "/nctucourse/static/media/logo256.33724e07.png"
+  },
+  {
+    "revision": "620650e3318b6f54ec07eb6f3d6be978",
+    "url": "/nctucourse/static/media/nctu_gpa_rule.620650e3.PNG"
+  },
+  {
+    "revision": "1551d230937149c7a663db10144fd4b7",
+    "url": "/nctucourse/static/media/p1.1551d230.PNG"
+  },
+  {
+    "revision": "432457cecb8603f20db1d50923001d40",
+    "url": "/nctucourse/static/media/p2.432457ce.PNG"
+  },
+  {
+    "revision": "59bd4520f01db3779eb70b49700abf93",
+    "url": "/nctucourse/static/media/p3.59bd4520.PNG"
+  },
+  {
+    "revision": "ba42fa1ff26faaaed5d95ed2d1553462",
+    "url": "/nctucourse/static/media/p4.ba42fa1f.PNG"
+  },
+  {
+    "revision": "89ecbafe8444131d2ecd1e192f49b9a9",
+    "url": "/nctucourse/static/media/p5.89ecbafe.PNG"
+  },
+  {
+    "revision": "3a56930f26c7be8a93a8405a72900310",
+    "url": "/nctucourse/static/media/p6.3a56930f.PNG"
+  },
+  {
+    "revision": "339de743fd7332a1362e2d96f2abd0c8",
+    "url": "/nctucourse/static/media/p7.339de743.PNG"
+  },
+  {
+    "revision": "02ee60b7e6f85d146fec41dce1667ffa",
+    "url": "/nctucourse/static/media/p8.02ee60b7.png"
+  }
+]);
